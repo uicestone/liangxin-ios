27823cc3b2cc33843842bbf4ea4f684054482ec0
supersheep <supersheepxjy@gmail.com>
2015-05-10 10:36:13 +0800

    新增LXWebViewController lol

---
+++ Liangxin/LXWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXWebViewController.h
+//  Liangxin
+//
+//  Created by Hsu Spud on 15/5/10.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXWebViewController : UIViewController
+
+@end
---
+++ Liangxin/LXWebViewController.m
@@ -0,0 +1,37 @@
+//
+//  LXWebViewController.m
+//  Liangxin
+//
+//  Created by Hsu Spud on 15/5/10.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import "LXWebViewController.h"
+
+@interface LXWebViewController ()
+
+@end
+
+@implementation LXWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
