c5bc695f19344f4d6ba0cceb2223e5aaf3ea790f
xiebohui <bohui.xie@dianping.com>
2015-05-28 22:35:23 +0800

    Add LXClassViewModel

---
+++ Liangxin/Class/ViewModel/LXClassViewModel.h
@@ -0,0 +1,13 @@
+//
+//  LXClassViewModel.h
+//  Liangxin
+//
+//  Created by xiebohui on 5/28/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXClassViewModel : NSObject
+
+@end
