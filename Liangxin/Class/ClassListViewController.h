bee86cd3058d055f41453703cc6281c775ebe0bf
xiebohui <bohui.xie@dianping.com>
2015-05-31 15:59:39 +0800

    Add ClassListViewController

---
+++ Liangxin/Class/ClassListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClassListViewController.h
+//  Liangxin
+//
+//  Created by xiebohui on 5/31/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseViewController.h"
+
+@interface ClassListViewController : LXBaseViewController
+
+@end
---
+++ Liangxin/Class/ClassListViewController.m
@@ -0,0 +1,21 @@
+//
+//  ClassListViewController.m
+//  Liangxin
+//
+//  Created by xiebohui on 5/31/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ClassListViewController.h"
+
+@interface ClassListViewController ()
+
+@end
+
+@implementation ClassListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
