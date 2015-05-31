16ebcd893e980e1c6c11d9e58eeac53b962e6876
xiebohui <bohui.xie@dianping.com>
2015-05-31 16:15:39 +0800

    修改工程配置

--- Liangxin/Class/ClassListViewController.m
+++ Liangxin/Class/ClassListViewController.m
@@ -8,7 +8,9 @@
 
 #import "ClassListViewController.h"
 
-@interface ClassListViewController ()
+@interface ClassListViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -16,6 +18,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (void)commonInit {
+    self.title = @"课堂列表";
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.tableView.separatorInset = UIEdgeInsetsZero;
+    self.tableView.layoutMargins = UIEdgeInsetsZero;
+    self.tableView.rowHeight = 75;
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(0);
+        make.right.mas_equalTo(0);
+        make.top.mas_equalTo(0);
+        make.bottom.mas_equalTo(0);
+    }];
+    self.tabBarController.tabBar.hidden = YES;
+}
+
+#pragma mark - UITableViewDataSource && UITableViewDelegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
 }
 
 @end
