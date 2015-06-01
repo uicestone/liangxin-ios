615b5754bfbd78a4460ee775d03d8a684e097081
xiebohui <xiebohui2014@163.com>
2015-06-01 21:32:44 +0800

    Add LXFilterView

---
+++ Liangxin/Widgets/LXFilterView.h
@@ -0,0 +1,13 @@
+//
+//  LXFilterView.h
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/1.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXFilterView : UIView
+
+@end
---
+++ Liangxin/Widgets/LXFilterView.m
@@ -0,0 +1,22 @@
+//
+//  LXFilterView.m
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/1.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import "LXFilterView.h"
+
+@implementation LXFilterView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
