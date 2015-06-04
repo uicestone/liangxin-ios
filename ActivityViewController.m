f2c1610d7a3038c16daa3560023356019fa02985
xiebohui <bohui.xie@dianping.com>
2015-06-04 16:20:42 +0800

    Add ActivityViewModel

--- ActivityViewController.m
+++ ActivityViewController.m
@@ -7,11 +7,47 @@
 //
 
 #import "ActivityViewController.h"
+#import "LXCarouselView.h"
+#import "LXBannerView.h"
+#import "LXBaseTableViewCell.h"
+
+@interface ActivityViewController() <UITableViewDataSource, UITableViewDelegate, LXBannerViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) LXCarouselView *carouselView;
+@property (nonatomic, strong) UIView *titleView;
+
+@end
 
 @implementation ActivityViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self commonInit];
+}
+
+- (void)commonInit {
+    self.title = @"精彩活动";
+}
+
+#pragma mark - LXBannerViewDelegate
+
+- (void)bannerView:(LXBannerView *)bannerView didSelectItemAtIndex:(NSInteger)index {
+    
+}
+
+#pragma mark - UITableViewDataSource && UITableViewDelegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
 }
 
 @end
---
+++ Liangxin/Activity/ViewModel/ActivityViewModel.h
@@ -0,0 +1,13 @@
+//
+//  ActivityViewModel.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/4/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActivityViewModel : NSObject
+
+@end
