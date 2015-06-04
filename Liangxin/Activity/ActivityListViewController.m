3be4f5ab2d54c2e99c081072e88bc4f8a4ca6a12
xiebohui <xiebohui2014@163.com>
2015-06-04 21:19:44 +0800

    Add ActivityListViewController

---
+++ Liangxin/Activity/ActivityListViewController.m
@@ -0,0 +1,21 @@
+//
+//  ActivityListViewController.m
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/4.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import "ActivityListViewController.h"
+
+@interface ActivityListViewController ()
+
+@end
+
+@implementation ActivityListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
