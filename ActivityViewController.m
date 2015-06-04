2c791366152d6d6787be121906427409689af76b
xiebohui <bohui.xie@dianping.com>
2015-06-04 17:14:33 +0800

    调整ActivityViewController

--- ActivityViewController.m
+++ ActivityViewController.m
@@ -52,12 +52,12 @@
         make.left.mas_equalTo(0);
         make.right.mas_equalTo(0);
         make.top.mas_equalTo(self.carouselView.mas_bottom);
-        make.height.mas_equalTo(85);
+        make.height.mas_equalTo(135);
     }];
     
     NSArray *channelTitles = @[@"最受欢迎", @"最新活动", @"即将下线", @"全部活动"];
     NSArray *channelImages = @[@"Banner_Favourite", @"Banner_New", @"Banner_All", @"Banner_Offline"];
-    CGFloat width = CGRectGetWidth([UIScreen mainScreen].bounds) / channelTitles.count;
+    CGFloat width = CGRectGetWidth([UIScreen mainScreen].bounds) / 2;
     for (NSInteger i = 0; i < channelTitles.count; i ++) {
         UIButton *channelView = [UIButton buttonWithType:UIButtonTypeCustom];
         [channelView setImage:[UIImage imageNamed:channelImages[i]] forState:UIControlStateNormal];
@@ -75,12 +75,22 @@
         CGSize titleSize = [channelView.titleLabel.text sizeWithAttributes:@{NSFontAttributeName: channelView.titleLabel.font}];
         channelView.imageEdgeInsets = UIEdgeInsetsMake(- (titleSize.height + spacing), 0.0, 0.0, - titleSize.width);
         [self.titleView addSubview:channelView];
-        [channelView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.mas_equalTo(0);
-            make.left.mas_equalTo(i * width);
-            make.width.mas_equalTo(width);
-            make.height.mas_equalTo(50);
-        }];
+        if (i == 0 || i == 1) {
+            [channelView mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.top.mas_equalTo(0);
+                make.left.mas_equalTo(i * width);
+                make.width.mas_equalTo(width);
+                make.height.mas_equalTo(50);
+            }];
+        }
+        else {
+            [channelView mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.top.mas_equalTo(50);
+                make.left.mas_equalTo((i - 2) * width);
+                make.width.mas_equalTo(width);
+                make.height.mas_equalTo(50);
+            }];
+        }
         channelView.rac_command = [[RACCommand alloc] initWithSignalBlock:^RACSignal *(id input) {
             return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
                 [subscriber sendCompleted];
@@ -91,14 +101,14 @@
     
     LXBannerView *bannerView = [LXBannerView new];
     bannerView.titles = @[@"爱摄影", @"做公益", @"文艺迷", @"体育狂", @"张知识", @"学环保"];
-    bannerView.images = @[@"Banner_DJ", @"Banner_QN", @"Banner_XC", @"Banner_FN", @"Banner_GH", @"Banner_LZ"];
+    bannerView.images = @[@"Banner_ISY", @"Banner_ZGY", @"Banner_WYM", @"Banner_TYK", @"Banner_ZZS", @"Banner_XHB"];
     bannerView.backgroundColor = UIColorFromRGB(0xf99d33);
     bannerView.delegate = self;
     [self.titleView addSubview:bannerView];
     [bannerView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.mas_equalTo(0);
         make.right.mas_equalTo(0);
-        make.top.mas_equalTo(55);
+        make.top.mas_equalTo(105);
         make.height.mas_equalTo(25);
     }];
     
