a8bd14c8cb65e7075cef66cfbf4a5f299a30aec9
xiebohui <xiebohui2014@163.com>
2015-06-04 23:45:54 +0800

    Add LXHomeViewController

---
+++ Liangxin/Home/LXHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXHomeViewController.h
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/4.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseViewController.h"
+
+@interface LXHomeViewController : LXBaseViewController
+
+@end
---
+++ Liangxin/Home/LXHomeViewController.m
@@ -0,0 +1,21 @@
+//
+//  LXHomeViewController.m
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/4.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import "LXHomeViewController.h"
+
+@interface LXHomeViewController ()
+
+@end
+
+@implementation LXHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
