5a9f2da7c2e76140b6725418d287f143f69bec54
xiebohui <bohui.xie@dianping.com>
2015-06-04 20:18:31 +0800

    Add ActivityDetailViewController

---
+++ Liangxin/Activity/ActivityDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailViewController.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/4/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseViewController.h"
+
+@interface ActivityDetailViewController : LXBaseViewController
+
+@end
---
+++ Liangxin/Activity/ActivityDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailViewController.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/4/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ActivityDetailViewController.h"
+
+@implementation ActivityDetailViewController
+
+@end
