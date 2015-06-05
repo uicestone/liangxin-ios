1af3c99e60902465bb38615f00e118fe9990318a
xiebohui <bohui.xie@dianping.com>
2015-06-05 09:39:12 +0800

    删除不需要的代码

--- Liangxin/AppDelegate.m
+++ Liangxin/AppDelegate.m
@@ -48,7 +48,7 @@
 
 #import "LXHomeViewController.h"
 
-@interface AppDelegate () <UITabBarControllerDelegate, UIActionSheetDelegate, LoginFinishDelegate>
+@interface AppDelegate () <LoginFinishDelegate>
 
 @end
 
@@ -58,28 +58,16 @@
 @synthesize window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
     
     window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-
-    
-    application.statusBarHidden = NO;
-    application.statusBarOrientation = UIDeviceOrientationPortrait;
-    
     
     // 初始化status bar
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-    [[UIApplication sharedApplication] setStatusBarHidden:NO];
     
     // 设置返回按钮样式
     [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0, -60) forBarMetrics:UIBarMetricsDefault];
     
-    // 初始化Router
-    
-    
     // 党群
-    
     [[HHRouter shared] map:@"/home" toControllerClass:[LXHomeViewController class]];
     [[HHRouter shared] map:@"/group" toControllerClass:[GroupViewController class]];
     [[HHRouter shared] map:@"/group/:id" toControllerClass:[GroupViewController class]];
@@ -88,8 +76,6 @@
     [[HHRouter shared] map:@"/groupactivity/:id" toControllerClass:[GroupPostViewController class]];
     [[HHRouter shared] map:@"/groupalbum/:id" toControllerClass:[GroupAlbumViewController class]];
     [[HHRouter shared] map:@"/groupmembers/:id" toControllerClass:[GroupMembersViewController class]];
-    
-    
     
     // 活动
     [[HHRouter shared] map:@"/activity" toControllerClass:[ActivityViewController class]];
@@ -149,65 +135,10 @@
     
     // 初始化Navigation Controller
     UIViewController *homeViewController = [[HHRouter shared] matchController:@"/home"];
-    
-    
-    // SecondViewController
-    navigationController= [[LXNavigationController alloc]
-                                             initWithRootViewController:homeViewController];
-    
+    navigationController= [[LXNavigationController alloc] initWithRootViewController:homeViewController];
     window.rootViewController = navigationController;
-    
     [window makeKeyAndVisible];
-    
     return YES;
-}
-
-
-- (BOOL)tabBarController:(UITabBarController *)tc shouldSelectViewController:(UIViewController *)vc{
-    int index = (int)[[tc viewControllers] indexOfObject:vc];
-    switch (index) {
-        case 0:
-            [[self tabBarController] setSelectedIndex:1];
-            [self.navigationController popToRootViewControllerAnimated:YES];
-            break;
-        case 1:
-            [self showPostBar];
-            break;
-        case 2:
-            
-            break;
-    }
-    if(index == 2){
-        return YES;
-    }else{
-        return NO;
-    }
-}
-
-#pragma ActionSheetDelegate
-- (void)showPostBar{
-    UIActionSheet *actionSheet = [[UIActionSheet alloc]
-                                  initWithTitle: nil
-                                  delegate:self
-                                  cancelButtonTitle:@"取消"
-                                  destructiveButtonTitle: nil
-                                  otherButtonTitles:@"公告", @"文章", @"照片", nil];
-    actionSheet.actionSheetStyle = UIActionSheetStyleBlackOpaque;
-    
-    actionSheet.delegate = self;
-    
-    
-    [actionSheet showInView:self.tabBarController.view];
-
-}
-
-
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
-    if(buttonIndex != 3){
-        NSString* type = [@[@"notice",@"article",@"image"] objectAtIndex:buttonIndex];
-        NSString* url = [@"publish/?type=" stringByAppendingString:type];
-        openURL(url);
-    }
 }
 
 -(void)loginFinished:(LXBaseViewController *)nextViewController{
@@ -269,25 +200,18 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
