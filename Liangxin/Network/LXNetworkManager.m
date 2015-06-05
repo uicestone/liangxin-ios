0cdf5eb6add4639da81fc511ae421106aab7a32c
xiebohui <bohui.xie@dianping.com>
2015-06-05 15:52:42 +0800

    Add LXNetworkManager

---
+++ Liangxin/Network/LXNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  LXNetworkManager.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/5/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXNetworkManager.h"
+
+@implementation LXNetworkManager
+
+@end
