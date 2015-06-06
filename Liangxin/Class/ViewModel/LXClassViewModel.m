c6ca32e56f730fb19418d083d8e4d149316be6d2
xiebohui <xiebohui2014@163.com>
2015-06-06 09:27:04 +0800

    修改课堂请求

--- Liangxin/Class/ViewModel/LXClassViewModel.m
+++ Liangxin/Class/ViewModel/LXClassViewModel.m
@@ -20,47 +20,9 @@
 {
     self = [super init];
     if (self) {
-        _sessionManager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://dangqun.malu.gov.cn/"]];
-        _sessionManager.responseSerializer = [[AFJSONResponseSerializer alloc] init];
         _classData = [NSMutableArray array];
     }
     return self;
-}
-
-- (RACSignal *)getClassBanners {
-    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        [self.sessionManager GET:@"/api/v1/post" parameters:@{@"type":@"横幅", @"banner_position":@"课堂"} success:^(NSURLSessionDataTask *task, id responseObject) {
-            NSMutableArray *posts = [NSMutableArray array];
-            for (NSDictionary *post in responseObject) {
-                [posts addObject:[LXBaseModelPost modelWithDictionary:post error:nil]];
-            }
-            [subscriber sendNext:posts];
-            [subscriber sendCompleted];
-        } failure:^(NSURLSessionDataTask *task, NSError *error) {
-            [subscriber sendError:error];
-        }];
-        return [RACDisposable disposableWithBlock:^{
-            
-        }];
-    }];
-}
-
-- (RACSignal *)getPostByPage:(NSInteger)pageNumber {
-    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        [self.sessionManager GET:@"/api/v1/post" parameters:@{@"type":@"课堂", @"page":@(pageNumber)} success:^(NSURLSessionDataTask *task, id responseObject) {
-            NSMutableArray *posts = [NSMutableArray array];
-            for (NSDictionary *post in responseObject) {
-                [posts addObject:[LXBaseModelPost modelWithDictionary:post error:nil]];
-            }
-            [subscriber sendNext:posts];
-            [subscriber sendCompleted];
-        } failure:^(NSURLSessionDataTask *task, NSError *error) {
-            [subscriber sendError:error];
-        }];
-        return [RACDisposable disposableWithBlock:^{
-            
-        }];
-    }];
 }
 
 @end
