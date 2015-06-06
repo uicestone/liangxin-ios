7f65ee36e2544fa8fa732a1cbfea525efa9b31fe
xiebohui <xiebohui2014@163.com>
2015-06-06 09:45:40 +0800

    修改BackButton

--- Liangxin/Base/LXBaseViewController.m
+++ Liangxin/Base/LXBaseViewController.m
@@ -25,10 +25,15 @@
     self.extendedLayoutIncludesOpaqueBars = YES;
     self.edgesForExtendedLayout = UIRectEdgeNone;
     self.navigationController.navigationBar.translucent = NO;
+    
     self.backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
     [self.backButton setImage:[UIImage imageNamed:@"Back"] forState:UIControlStateNormal];
     self.backButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
     [self.backButton addTarget:self action:@selector(back:) forControlEvents:UIControlEventTouchUpInside];
+    if (!self.params[@"route"]) {
+        self.backButton.imageView.tintColor = [UIColor blueColor];
+    }
+    
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.backButton];
     self.currentUser = [UserApi getCurrentUser];
 }
