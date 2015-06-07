dca6dff75b94eb9ec946a0cf3813c09a67505f41
xiebohui <xiebohui2014@163.com>
2015-06-07 16:40:56 +0800

    Add LXSearchBar

---
+++ Liangxin/Widgets/LXSearchBar.h
@@ -0,0 +1,15 @@
+//
+//  LXSearchBar.h
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/7.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXSearchBar : UIView
+
+@property (nonatomic, strong) UITextField *searchField;
+
+@end
---
+++ Liangxin/Widgets/LXSearchBar.m
@@ -0,0 +1,28 @@
+//
+//  LXSearchBar.m
+//  Liangxin
+//
+//  Created by xiebohui on 15/6/7.
+//  Copyright (c) 2015年 Hsu Spud. All rights reserved.
+//
+
+#import "LXSearchBar.h"
+
+@interface LXSearchBar()
+
+@property (nonatomic, strong) UIButton *searchIcon;
+@property (nonatomic, strong) UIButton *searchButton;
+
+@end
+
+@implementation LXSearchBar
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
