c6336798ec381cffb5fa874cc27627cb16078184
xiebohui <bohui.xie@dianping.com>
2015-06-08 17:29:57 +0800

    添加部分UI

--- Liangxin/Activity/Views/ActivityParticipantCell.m
+++ Liangxin/Activity/Views/ActivityParticipantCell.m
@@ -8,6 +8,25 @@
 
 #import "ActivityParticipantCell.h"
 
+@interface ActivityParticipantCell()
+
+@property (nonatomic, strong) UIImageView *avatarImageView;
+@property (nonatomic, strong) UILabel *userStateLabel;
+@property (nonatomic, strong) UIButton *agreeButton;
+@property (nonatomic, strong) UIButton *disagreeButton;
+@property (nonatomic, strong) UILabel *nameLabel;
+@property (nonatomic, strong) UILabel *groupLabel;
+
+@end
+
 @implementation ActivityParticipantCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
 
 @end
