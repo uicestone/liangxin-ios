10d5987b9b220c99707972af9779222140d79e65
xiebohui <bohui.xie@dianping.com>
2015-06-08 17:21:23 +0800

    Add ActivityParticipantCell

---
+++ Liangxin/Activity/Views/ActivityParticipantCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityParticipantCell.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/8/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityParticipantCell : UITableViewCell
+
+@end
---
+++ Liangxin/Activity/Views/ActivityParticipantCell.m
@@ -0,0 +1,13 @@
+//
+//  ActivityParticipantCell.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/8/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ActivityParticipantCell.h"
+
+@implementation ActivityParticipantCell
+
+@end
