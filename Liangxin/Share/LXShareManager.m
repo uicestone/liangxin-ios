c039fe022b6c21eaaac227375330255072421f09
xiebohui <bohui.xie@dianping.com>
2015-06-12 11:00:45 +0800

    Add LXShareManager

---
+++ Liangxin/Share/LXShareManager.m
@@ -0,0 +1,13 @@
+//
+//  LXShareManager.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/12/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXShareManager.h"
+
+@implementation LXShareManager
+
+@end
