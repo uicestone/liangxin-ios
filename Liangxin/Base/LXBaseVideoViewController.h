73c0a49f57123a542f9159e65e68a938ea1f0f20
xiebohui <bohui.xie@dianping.com>
2015-06-14 19:21:33 +0800

    Add LXBaseVideoViewController

---
+++ Liangxin/Base/LXBaseVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXBaseVideoViewController.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/14/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseViewController.h"
+
+@interface LXBaseVideoViewController : LXBaseViewController
+
+@end
---
+++ Liangxin/Base/LXBaseVideoViewController.m
@@ -0,0 +1,37 @@
+//
+//  LXBaseVideoViewController.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/14/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseVideoViewController.h"
+
+@interface LXBaseVideoViewController ()
+
+@end
+
+@implementation LXBaseVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
