8073ef12d86140560d8fdc3494f946d63b779b3d
xiebohui <bohui.xie@dianping.com>
2015-06-15 15:00:53 +0800

    Add ActivityDetailDescCell

---
+++ Liangxin/Activity/Views/ActivityDetailDescCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailDescCell.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/15/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ClassDetailBaseCell.h"
+
+@interface ActivityDetailDescCell : ClassDetailBaseCell
+
+@end
---
+++ Liangxin/Activity/Views/ActivityDetailDescCell.m
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailDescCell.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/15/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ActivityDetailDescCell.h"
+
+@implementation ActivityDetailDescCell
+
+@end
