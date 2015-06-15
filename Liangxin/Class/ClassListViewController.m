5912decced16f1671a02763406867aa4aae96a76
xiebohui <bohui.xie@dianping.com>
2015-06-15 23:00:01 +0800

    添加筛选回调

--- Liangxin/Class/ClassListViewController.m
+++ Liangxin/Class/ClassListViewController.m
@@ -12,7 +12,7 @@
 #import "LXFilterView.h"
 #import "ClassDetailViewController.h"
 
-@interface ClassListViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface ClassListViewController () <UITableViewDataSource, UITableViewDelegate, LXFilterViewDelegate>
 
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) LXClassListViewModel *viewModel;
@@ -46,6 +46,7 @@
     self.filterView.tintColor = UIColorFromRGB(0xf99d33);
     self.filterView.category1 = @[@"智能筛选", @"党建", @"青年", @"宣传", @"妇女", @"工会", @"廉政"];
     self.filterView.category2 = @[@"类别", @"最受欢迎课堂", @"最新课堂", @"全部课堂"];
+    self.filterView.delegate = self;
     [self.view addSubview:self.filterView];
     LXNetworkPostParameters *parameters = [LXNetworkPostParameters new];
     parameters.type = @"课堂";
@@ -54,6 +55,44 @@
     }
     if ([self.params objectForKey:@"order_by"]) {
         parameters.class_type = [[self.params objectForKey:@"order_by"] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    }
+    @weakify(self)
+    [[[LXNetworkManager sharedManager] getPostByParameters:parameters] subscribeNext:^(NSArray *posts) {
+        @strongify(self)
+        [self.viewModel.listData addObjectsFromArray:posts];
+        [self.tableView reloadData];
+    } error:^(NSError *error) {
+        
+    }];
+}
+
+#pragma mark - LXFilterViewDelegate
+
+- (void)filterView:(LXFilterView *)filterView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    LXNetworkPostParameters *parameters = [LXNetworkPostParameters new];
+    parameters.type = @"课堂";
+    if (indexPath.section == 1) {
+        parameters.class_type = [filterView.category1 objectAtIndex:indexPath.row - 1];
+    }
+    else if (indexPath.section == 2) {
+        switch (indexPath.row) {
+            case 0:
+                break;
+            case 1: {
+                parameters.order_by = @"likes";
+            }
+                break;
+            case 2: {
+                parameters.order_by = @"updated_at";
+            }
+                break;
+            case 3: {
+                
+            }
+                break;
+            default:
+                break;
+        }
     }
     @weakify(self)
     [[[LXNetworkManager sharedManager] getPostByParameters:parameters] subscribeNext:^(NSArray *posts) {
--- Liangxin/Widgets/LXFilterView.h
+++ Liangxin/Widgets/LXFilterView.h
@@ -8,7 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+@class LXFilterView;
+
+@protocol LXFilterViewDelegate <NSObject>
+
+- (void)filterView:(LXFilterView *)filterView didSelectItemAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
+
 @interface LXFilterView : UIView
+
+@property (nonatomic, weak) id<LXFilterViewDelegate> delegate;
 
 @property (nonatomic, strong) NSArray *category1;
 @property (nonatomic, strong) NSArray *category2;
