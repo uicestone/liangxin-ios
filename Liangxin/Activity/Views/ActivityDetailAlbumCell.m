ddb6eb1fe6ec95a7cb5d19478ed94541e8feddcd
xiebohui <bohui.xie@dianping.com>
2015-06-15 15:11:08 +0800

    Add ActivityDetailAlbumCell

---
+++ Liangxin/Activity/Views/ActivityDetailAlbumCell.m
@@ -0,0 +1,13 @@
+//
+//  ActivityDetailAlbumCell.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/15/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ActivityDetailAlbumCell.h"
+
+@implementation ActivityDetailAlbumCell
+
+@end
