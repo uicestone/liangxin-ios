d4caf4f60a994502f7ae6f11c63e8092914a1f51
xiebohui <bohui.xie@dianping.com>
2015-06-15 13:58:45 +0800

    Add ActivityDetailTitleCell

---
+++ Liangxin/Activity/Views/ActivityDetailTitleCell.m
@@ -0,0 +1,17 @@
+//
+//  ActivityDetailTitleCell.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/15/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ActivityDetailTitleCell.h"
+
+@implementation ActivityDetailTitleCell
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
