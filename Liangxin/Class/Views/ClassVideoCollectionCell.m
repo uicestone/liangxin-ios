9901a067c8f6f69d94534138084f155803c2846f
xiebohui <bohui.xie@dianping.com>
2015-06-16 09:56:32 +0800

    修改视频列表

--- Liangxin/Class/Views/ClassVideoCollectionCell.m
+++ Liangxin/Class/Views/ClassVideoCollectionCell.m
@@ -11,7 +11,7 @@
 @interface ClassVideoCollectionCell()
 
 @property (nonatomic, strong) UILabel *titleLabel;
-@property (nonatomic, strong) UIButton *videoButton;
+@property (nonatomic, strong) UIView *videoView;
 
 @end
 
@@ -20,22 +20,29 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        _videoButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        _videoButton.backgroundColor = [UIColor colorWithRed:169/255.0 green:171.0/255.0 blue:174.0/255.0 alpha:1.0];
-        [_videoButton setImage:[UIImage imageNamed:@"Video"] forState:UIControlStateNormal];
-        [self addSubview:_videoButton];
-        [_videoButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        _videoView = [UIView new];
+        _videoView.backgroundColor = [UIColor colorWithRed:169/255.0 green:171.0/255.0 blue:174.0/255.0 alpha:1.0];
+        [self addSubview:_videoView];
+        [_videoView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(0);
             make.right.mas_equalTo(0);
             make.top.mas_equalTo(0);
             make.bottom.mas_equalTo(0);
+        }];
+        UIImageView *videoImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Video"]];
+        [self.videoView addSubview:videoImageView];
+        [videoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerX.equalTo(self.videoView.mas_centerX);
+            make.centerY.equalTo(self.videoView.mas_centerY);
+            make.width.mas_equalTo(25);
+            make.height.mas_equalTo(25);
         }];
         _titleLabel = [UILabel new];
         _titleLabel.textAlignment = NSTextAlignmentCenter;
         _titleLabel.font = [UIFont systemFontOfSize:13.0];
         _titleLabel.textColor = [UIColor whiteColor];
         _titleLabel.backgroundColor = [UIColor colorWithRed:80/255.0 green:80/255.0 blue:80/255.0 alpha:1.0];
-        [_videoButton addSubview:_titleLabel];
+        [_videoView addSubview:_titleLabel];
         [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(0);
             make.right.mas_equalTo(0);
@@ -48,9 +55,6 @@
 
 - (void)reloadViewWithData:(LXBaseModelPost *)data {
     self.titleLabel.text = data.title?:@"";
-    if (data.url.length > 0) {
-        [self.videoButton sd_setBackgroundImageWithURL:[NSURL URLWithString:data.url] forState:UIControlStateNormal];
-    }
 }
 
 @end
