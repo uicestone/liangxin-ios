e73161ffd98bedc795c2c55a5205fb220f462a86
xiebohui <bohui.xie@dianping.com>
2015-06-17 19:41:39 +0800

    Add ClassDetalViewController

---
+++ Liangxin/Class/ClassDetalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClassDetalViewController.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/17/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseViewController.h"
+
+@interface ClassDetalViewController : LXBaseViewController
+
+@end
---
+++ Liangxin/Class/ClassDetalViewController.m
@@ -0,0 +1,21 @@
+//
+//  ClassDetalViewController.m
+//  Liangxin
+//
+//  Created by xiebohui on 6/17/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "ClassDetalViewController.h"
+
+@interface ClassDetalViewController ()
+
+@end
+
+@implementation ClassDetalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
