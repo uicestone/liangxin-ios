43d7648908b4ae4bb470a177a02acdc83181bef4
xiebohui <bohui.xie@dianping.com>
2015-06-17 10:29:04 +0800

    Add LXImageViewerController

---
+++ Liangxin/Widgets/LXImageViewerController.h
@@ -0,0 +1,13 @@
+//
+//  LXImageViewerController.h
+//  Liangxin
+//
+//  Created by xiebohui on 6/17/15.
+//  Copyright (c) 2015 Hsu Spud. All rights reserved.
+//
+
+#import "LXBaseViewController.h"
+
+@interface LXImageViewerController : LXBaseViewController
+
+@end
